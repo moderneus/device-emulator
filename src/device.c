0f8b5d53bebc7b2fd4aaf53d504d222cdcc64903
moderneus <finelefrinchik@gmail.com>
2026-02-20 18:41:08 +0400

    fix: add heap is not zero warning and zeroing device fields in device_destroy()

--- src/device.c
+++ src/device.c
@@ -14,5 +14,8 @@ void device_init(struct device *dev)
 void device_destroy(struct device *dev)
 {
 	warn(dev->handle == DEVICE_NONE_HANDLE, "device is not inited");
+	warn(dev->heap != NULL, "device heap pointer is not NULL");
 	dev->handle = DEVICE_NONE_HANDLE;
+	dev->heap = NULL;
+	dev->heap_len = 0;
 }
