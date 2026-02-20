dd694e1e5dbff4e8c7360bd48497f552c7ea67e7
moderneus <moderneusf@gmail.com>
2026-02-20 22:13:32 +0400

    feat: add test file with basic use of device funcs

---
+++ src/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include "device.h"
+#include "mem.h"
+#include "def.h"
+#include "io.h"
+
+int main(void)
+{
+	struct device dev;
+	u32 buf[4];
+	u32 i;
+
+	device_init(&dev);
+	device_malloc(&dev, sizeof(u32) * 4);
+
+	for(i = 0; i < dev.heap_len / sizeof(u32); ++i)
+		device_write(&dev, i, );
+
+	for(i = 0; i < dev.heap_len / sizeof(u32); ++i)
+		device_read(&dev, &buf[i], i);
+
+	for(i = 0; i < sizeof(buf) / sizeof(buf[0]); ++i)
+		printf("buf[%i] = %i\n", i, buf[i]);
+
+	device_free(&dev);
+	device_destroy(&dev);
+
+	return 0;
+}
