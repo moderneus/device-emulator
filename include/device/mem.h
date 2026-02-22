0795043d4b684e2bf141aebb66719a49f584ce1e
moderneus <moderneusf@gmail.com>
2026-02-22 11:19:58 +0400

    feat: add device_calloc() and device_realloc() funcs

--- include/device/mem.h
+++ include/device/mem.h
@@ -7,6 +7,10 @@ struct device;
 
 void device_malloc(struct device *dev, u32 len);
 
+void device_calloc(struct device *dev, u32 len);
+
+void device_realloc(struct device *dev, u32 len);
+
 void device_free(struct device *dev);
 
 #endif
--- src/mem.c
+++ src/mem.c
@@ -18,6 +18,31 @@ void device_malloc(struct device *dev, u32 len)
 	err(!dev->heap, "failed to alloc mem");
 }
 
+void device_calloc(struct device *dev, u32 len)
+{
+	err(dev->handle == DEVICE_NONE_HANDLE, "device is not inited");
+	warn(dev->heap != NULL, "device already has allocated mem");
+
+	if (dev->heap)
+		free(dev->heap);
+	
+	dev->heap_len = len;
+	dev->heap = calloc(dev->heap_len / sizeof(((u32*)dev->heap)[0]), sizeof(((u32*)dev->heap)[0]));
+
+	err(!dev->heap, "failed to calloc mem");
+}
+
+void device_realloc(struct device *dev, u32 len) 
+{
+	err(dev->handle == DEVICE_NONE_HANDLE, "device is not inited");
+	warn(dev->heap == NULL, "device has not allocated mem");
+
+	dev->heap_len = len;
+	dev->heap = realloc(dev->heap, dev->heap_len);
+
+	err(!dev->heap, "failed to realloc mem");
+}
+
 void device_free(struct device *dev)
 {
 	err(dev->handle == DEVICE_NONE_HANDLE, "device is not inited");
